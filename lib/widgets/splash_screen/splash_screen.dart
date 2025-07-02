c03588649b210d6922d811070c10a7fd750d734d
VennyS <panarin.danil.08@gmail.com>
2025-07-02 11:11:54 +0500

    feat: configured native splash screens for android and ios; added in splash screen widget logo with opacity animation

--- lib/widgets/splash_screen/splash_screen.dart
+++ lib/widgets/splash_screen/splash_screen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+
+    _animation = Tween(
+      begin: 0.4,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xFFEFF6FF),
-      child: const Center(
-        child: CircularProgressIndicator(color: Color(0xFF2563EB)),
+      child: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Image.asset("assets/images/logo_above_text.png", scale: 4),
+        ),
       ),
     );
   }
